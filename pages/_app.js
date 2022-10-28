f3c5216c1edf42ba48d876aeafe6501c7e90bbf8
Ken Huang <bluetch@gmail.com>
2022-10-28 16:13:57 +0800

    feat: GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import { memo } from "react";
 import Head from "next/head";
-import { Header, Footer } from "components";
+import { useEffect } from "react";
+import TagManager from 'react-gtm-module';
 import { AppWrapper } from "contexts";
 import "styles/globals.css";
 
 const App = ({ Component, pageProps }) => {
   const MemoComponent = memo(Component);
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-MWXSCRM' });
+  }, []);
   return (
     <AppWrapper>
       <Head>
