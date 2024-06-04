fe8b20e588a7b884cdf52b48a67f94db61aa1f80
Ken Huang <bluetch@gmail.com>
2024-06-05 06:36:35 +0800

    add article

--- pages/api/articles/index.js
+++ pages/api/articles/index.js
@@ -1,6 +1,15 @@
 const Articles = (req, res) => {
   const articles = [
     {
+      name: "Reflection of Nvidia CEO Jensen Huang keynote at Computex 2024",
+      category: [],
+      desc: "Reflection of Nvidia CEO Jensen Huang keynote at Computex 2024.",
+      url: "https://medium.com/@bluetch/reflection-of-nvidia-ceo-jensen-huang-keynote-at-computex-2024-d61a6ba61475",
+      img: "https://miro.medium.com/v2/resize:fit:640/format:webp/1*YwL7B27LAlA2oXhKk29Uvw.png",
+      code: "20240605",
+      date: 20240605,
+    },
+    {
       name: "JavaScript from ES6 to ES12 in 2022",
       category: [],
       desc: "Javascript a one of the three most crucial programming at front-end positions. However, the author Brendan Eich just spent 10 days on it. It helps a lot in the website technically, but it also caused many issues to fix, that’s why there are from ES6(2015) to ES12(2021) born to fix better JavaScript.",
@@ -45,15 +54,15 @@ const Articles = (req, res) => {
       code: "20180528",
       date: 20180528,
     },
-    {
-      name: "讀書心得 — 《UX策略：設計讓人夢寐以求的創新數位產品》",
-      category: [],
-      desc: "繼上一本⟪UX從新手開始⟫讀完後，找到這本比較進階的內容，同樣是O’REILLY出版。會說進階，是因為跟新手那本學習門檻明顯高了也許，而且也比較專注在研究方法。裡面專案範例偏向中小型，適合給新創公司類型的設計師讀。",
-      url: "https://medium.com/@bluetch/%E8%AE%80%E6%9B%B8%E5%BF%83%E5%BE%97-ux%E7%AD%96%E7%95%A5-%E8%A8%AD%E8%A8%88%E8%AE%93%E4%BA%BA%E5%A4%A2%E5%AF%90%E4%BB%A5%E6%B1%82%E7%9A%84%E5%89%B5%E6%96%B0%E6%95%B8%E4%BD%8D%E7%94%A2%E5%93%81-362f5a4c3083",
-      img: "https://miro.medium.com/max/960/1*izxYql_CTOMmv7E3_mKKJQ.jpeg",
-      code: "20170831",
-      date: 20170831,
-    },
+    // {
+    //   name: "讀書心得 — 《UX策略：設計讓人夢寐以求的創新數位產品》",
+    //   category: [],
+    //   desc: "繼上一本⟪UX從新手開始⟫讀完後，找到這本比較進階的內容，同樣是O’REILLY出版。會說進階，是因為跟新手那本學習門檻明顯高了也許，而且也比較專注在研究方法。裡面專案範例偏向中小型，適合給新創公司類型的設計師讀。",
+    //   url: "https://medium.com/@bluetch/%E8%AE%80%E6%9B%B8%E5%BF%83%E5%BE%97-ux%E7%AD%96%E7%95%A5-%E8%A8%AD%E8%A8%88%E8%AE%93%E4%BA%BA%E5%A4%A2%E5%AF%90%E4%BB%A5%E6%B1%82%E7%9A%84%E5%89%B5%E6%96%B0%E6%95%B8%E4%BD%8D%E7%94%A2%E5%93%81-362f5a4c3083",
+    //   img: "https://miro.medium.com/max/960/1*izxYql_CTOMmv7E3_mKKJQ.jpeg",
+    //   code: "20170831",
+    //   date: 20170831,
+    // },
   ];
   return res.status(200).json(articles);
 };
