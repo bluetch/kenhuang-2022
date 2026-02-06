f7528d0a1d8d7ae3e38664164a258a18bf40fda9
Ken Huang <bluetch@mail.com>
2026-02-06 10:05:28 +0800

    upgrade next 16

--- components/Icon.js
+++ components/Icon.js
@@ -1,29 +1,48 @@
-import dynamic from "next/dynamic";
 import { memo } from "react";
 
-const IconNameOptions = Object.freeze([
-  "facebook",
-  "github",
-  "instagram",
-  "linkedin",
-  "medium",
-  "portfolio",
-  "refresh",
-  "smile",
-  "tools",
-  "work",
-]);
+import FacebookIcon from "../assets/icons/facebook.svg";
+import GithubIcon from "../assets/icons/github.svg";
+import InstagramIcon from "../assets/icons/instagram.svg";
+import LinkedinIcon from "../assets/icons/linkedin.svg";
+import MediumIcon from "../assets/icons/medium.svg";
+import PortfolioIcon from "../assets/icons/portfolio.svg";
+import RefreshIcon from "../assets/icons/refresh.svg";
+import SmileIcon from "../assets/icons/smile.svg";
+import ToolsIcon from "../assets/icons/tools.svg";
+import WorkIcon from "../assets/icons/work.svg";
+
+// 靜態映射
+const IconMap = {
+  facebook: FacebookIcon,
+  github: GithubIcon,
+  instagram: InstagramIcon,
+  linkedin: LinkedinIcon,
+  medium: MediumIcon,
+  portfolio: PortfolioIcon,
+  refresh: RefreshIcon,
+  smile: SmileIcon,
+  tools: ToolsIcon,
+  work: WorkIcon,
+};
+
+const IconNameOptions = Object.freeze(Object.keys(IconMap));
 
 const Icon = memo(({ name, ...rest }) => {
-  if (!IconNameOptions.includes(name)) {
-    throw new Error(`${name} is not defined in IconNameOptions`);
+  const Component = IconMap[name];
+  if (!Component) throw new Error(`${name} not defined in IconNameOptions`);
+
+  // When using Turbopack / Next's asset handling, importing an SVG may return
+  // a structured image object like `{ src, width, height }` instead of a
+  // React component. Handle both cases: render an <img> for image objects,
+  // otherwise render the component directly.
+  if (Component && typeof Component === "object" && Component.src) {
+    const { src } = Component;
+    return <img src={src} alt={name} {...rest} />;
   }
 
-  const DynamicIcon = dynamic(() => import(`/assets/icons/${name}.svg`));
-
-  return <DynamicIcon {...rest} />;
+  return <Component {...rest} />;
 });
 
 Icon.displayName = "Icon";
 
-export { IconNameOptions, Icon };
+export { Icon, IconNameOptions };
--- next.config.js
+++ next.config.js
@@ -1,11 +1,24 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: false,
+  turbopack: {}, // 啟用 Turbopack
   webpack(config) {
+    // Exclude .svg from Next.js default asset handling so we can use @svgr/webpack
+    const assetRule = config.module.rules.find((rule) => {
+      return rule && rule.test && rule.test.test && rule.test.test('.svg');
+    });
+    if (assetRule) {
+      assetRule.exclude = /\.svg$/i;
+    }
+
     config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
     });
 
     return config;
   },
 };
+
+module.exports = nextConfig;
